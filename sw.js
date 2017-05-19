5998119354bee4402ba1f5c567d7589ccd2b32c2
quasarchimaere <sudafabian@gmail.com>
2017-05-19 10:58:21 +0200

    added files to be cached by service worker, and service worker fetch from cache

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 var cacheName= "testCacheName";
-var filesToCache = [];
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/styles/style.css',
+    'img/circle.png',
+    'img/triangle.png',
+    'img/squiggle.png',
+    'img/square.png'
+];
 
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
@@ -7,6 +15,30 @@ self.addEventListener('install', function(e) {
         caches.open(cacheName).then(function(cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
     );
 });
